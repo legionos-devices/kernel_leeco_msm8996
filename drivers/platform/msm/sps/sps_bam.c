34c26e2e2e75f3c21fecb0b22c49857c61d464f2
Wei Wang <wvw@google.com>
2016-09-06 15:40:55 -0700

    sps: add spinlock protection for pipes_q being tainted
    Bug: 31119496
    Change-Id: I7d71f58e37d5ea85f15625078ab0d6cfa0362bff
    Signed-off-by: SJD Ayy <3xp1r4t3d@gmail.com>

--- drivers/platform/msm/sps/sps_bam.c
+++ drivers/platform/msm/sps/sps_bam.c
@@ -825,6 +825,7 @@ int sps_bam_pipe_connect(struct sps_pipe *bam_pipe,
 	void *desc_buf = NULL;
 	u32 pipe_index;
 	int result;
+	unsigned long flags;
 
 	/* Clear the client pipe state and hw init struct */
 	pipe_clear(bam_pipe);
@@ -1061,8 +1062,10 @@ int sps_bam_pipe_connect(struct sps_pipe *bam_pipe,
 
 	/* Indicate initialization is complete */
 	dev->pipes[pipe_index] = bam_pipe;
+	spin_lock_irqsave(&dev->isr_lock, flags);
 	dev->pipe_active_mask |= 1UL << pipe_index;
 	list_add_tail(&bam_pipe->list, &dev->pipes_q);
+	spin_unlock_irqrestore(&dev->isr_lock, flags);
 
 	SPS_DBG2(dev,
 		"sps:BAM %pa; pipe %d; pipe_index_mask:0x%x; pipe_active_mask:0x%x.\n",
@@ -2363,6 +2366,7 @@ int sps_bam_get_free_count(struct sps_bam *dev, u32 pipe_index,
 int sps_bam_set_satellite(struct sps_bam *dev, u32 pipe_index)
 {
 	struct sps_pipe *pipe = dev->pipes[pipe_index];
+	unsigned long flags;
 
 	/*
 	 * Switch to satellite control is only supported on processor
@@ -2404,8 +2408,10 @@ int sps_bam_set_satellite(struct sps_bam *dev, u32 pipe_index)
 	}
 
 	/* Indicate satellite control */
+	spin_lock_irqsave(&dev->isr_lock, flags);
 	list_del(&pipe->list);
 	dev->pipe_active_mask &= ~(1UL << pipe_index);
+	spin_unlock_irqrestore(&dev->isr_lock, flags);
 	dev->pipe_remote_mask |= pipe->pipe_index_mask;
 	pipe->state |= BAM_STATE_REMOTE;
 
