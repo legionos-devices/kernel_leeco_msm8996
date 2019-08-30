a665fd1f078847c46fc089b558e76fe0a5d62407
celtare21 <celtare21@gmail.com>
2019-08-30 18:26:13 +0000

    base: power: Shut up already
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -809,7 +809,7 @@ void pm_print_active_wakeup_sources(void)
 	rcu_read_lock();
 	list_for_each_entry_rcu(ws, &wakeup_sources, entry) {
 		if (ws->active) {
-			pr_info("active wakeup source: %s\n", ws->name);
+			pr_debug("active wakeup source: %s\n", ws->name);
 #ifdef CONFIG_BOEFFLA_WL_BLOCKER
 			if (!check_for_block(ws))	// AP: check if wakelock is on wakelock blocker list
 #endif
